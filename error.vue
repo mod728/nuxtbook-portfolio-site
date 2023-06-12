6607a42c7efc5d315663371fa3e3cfe81aabfa1e
mod728 <vinnybeach712@yahoo.co.jp>
2023-06-12 12:27:58 +0900

    Code for Chapter 5

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="error container">
+        <h1>404: Not Found</h1>
+        <p>ページが見つかりません。</p>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    title: "エラーページ",
+    meta: [
+        { name: "description", content: "エラーページです" }
+    ],
+})
+</script>
